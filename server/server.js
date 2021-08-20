3dcf81e31af0953e17f48aad22ccf3de2ff4a8b1
endless <73157473+endlessXD@users.noreply.github.com>
2021-08-20 15:24:42 -0700

    ok
    last one I swear

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const { port } = require('../config.json');
+
+if (!port) return;
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200); res.write('OK'); res.end();
 });
 
-server.listen(port, () => console.log('Server Listening on Port ' + port || 8000));
+server.listen(port, () => console.log('Server Listening on Port ' + port));
