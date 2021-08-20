bb910deae772f6b5d5fd2b6cf254c74149240776
endless <73157473+endlessXD@users.noreply.github.com>
2021-08-20 13:13:07 -0700

    pooooooooooooooooooort
    hi

--- server/server.js
+++ server/server.js
@@ -5,4 +5,4 @@ const server = http.createServer((req, res) => {
   res.writeHead(200); res.write('OK'); res.end();
 });
 
-server.listen(port, () => console.log('Server Listening on Port ' + port));
+server.listen(port, () => console.log('Server Listening on Port ' + port || 8000));
